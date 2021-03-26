fe090b2b431c4b3775b7ef6466ab3e12bbce0511
Izzy Swart <zenerboson@gmail.com>
2021-03-26 04:18:04 -0700

    shift indices when erasing lambdas

--- src/term/normalize.rs
+++ src/term/normalize.rs
@@ -136,6 +136,7 @@ impl Term {
             Lambda { body, erased, .. } => {
                 body.normalize(definitions)?;
                 if *erased {
+                    body.substitute_top(&Term::Variable(Index::top()));
                     *self = replace(&mut *body, Universe);
                 }
             }
