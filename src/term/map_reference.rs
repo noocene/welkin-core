7838182f3408b9a35a859ebd46108320ecaeefea
Izzy Swart <zenerboson@gmail.com>
2021-03-27 21:13:55 -0700

    avoid constructing an infinite function type

--- src/term/map_reference.rs
+++ src/term/map_reference.rs
@@ -3,7 +3,7 @@ use std::convert::Infallible;
 use super::Term;
 
 impl<T> Term<T> {
-    pub fn try_map_reference<U, E, F: Fn(T) -> Result<Term<U>, E>>(
+    pub fn try_map_reference<U, E, F: Fn(T) -> Result<Term<U>, E> + Clone>(
         self,
         f: F,
     ) -> Result<Term<U>, E> {
@@ -20,13 +20,13 @@ impl<T> Term<T> {
                 argument,
                 erased,
             } => Apply {
-                function: Box::new(function.try_map_reference(&f)?),
+                function: Box::new(function.try_map_reference(f.clone())?),
                 argument: Box::new(argument.try_map_reference(f)?),
                 erased,
             },
             Put(term) => Put(Box::new(term.try_map_reference(f)?)),
             Duplicate { expression, body } => Duplicate {
-                expression: Box::new(expression.try_map_reference(&f)?),
+                expression: Box::new(expression.try_map_reference(f.clone())?),
                 body: Box::new(body.try_map_reference(f)?),
             },
             Reference(reference) => f(reference)?,
@@ -36,7 +36,7 @@ impl<T> Term<T> {
                 return_type,
                 erased,
             } => Function {
-                argument_type: Box::new(argument_type.try_map_reference(&f)?),
+                argument_type: Box::new(argument_type.try_map_reference(f.clone())?),
                 return_type: Box::new(return_type.try_map_reference(f)?),
                 erased,
             },
@@ -45,14 +45,14 @@ impl<T> Term<T> {
                 expression,
                 ty,
             } => Annotation {
-                expression: Box::new(expression.try_map_reference(&f)?),
+                expression: Box::new(expression.try_map_reference(f.clone())?),
                 ty: Box::new(ty.try_map_reference(f)?),
                 checked,
             },
             Wrap(term) => Wrap(Box::new(term.try_map_reference(f)?)),
         })
     }
-    pub fn map_reference<U, F: Fn(T) -> Term<U>>(self, f: F) -> Term<U> {
+    pub fn map_reference<U, F: Clone + Fn(T) -> Term<U>>(self, f: F) -> Term<U> {
         self.try_map_reference(|a| Ok::<_, Infallible>(f(a)))
             .unwrap()
     }
